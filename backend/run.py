dcdc1c116f9e3ecb7f867bc450ec78c73dbdb812
hanzhe <sith0008@e.ntu.edu.sg>
2021-01-24 12:14:14 +0800

    setup docker for backend and mysql

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,10 @@
-from app import supa
-from app.routes import *
+from app import supa # noqa
+from app.routes import * # noqa
+import os
 
 if __name__ == "__main__":
     supa.config.from_pyfile('../config.py')
-    supa.run(host='127.0.0.1', port=5000, debug=True)
+    supa.run(
+        host=os.environ.get("BACKEND_HOST", "127.0.0.1"),
+        port=os.environ.get("BACKEND_PORT", 5000)
+    )
