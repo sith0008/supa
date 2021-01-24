746f17d630aa3f12c7fbe5d605546640f86e63a7
hanzhe <sith0008@e.ntu.edu.sg>
2021-01-24 19:49:13 +0800

    add TODO for location model

--- backend/app/models/location.py
+++ backend/app/models/location.py
@@ -1,6 +1,11 @@
 from py2neo.ogm import Model, Property, RelatedTo
 from app.models.property_type import SpecificPropType # noqa
 
+# TODO: add fields for PAs, PTAs, AGUs
+# PA: https://www.ura.gov.sg/Corporate/Property/Business/Change-Use-of-Property-for-Business/related/Problematic-Areas
+# PTA: https://www.ura.gov.sg/Corporate/Guidelines/Circulars/dc16-10#appendixa
+
+# TODO: create new classes to represent location hierarchy (specific location -> subzone -> district -> region)
 
 class Location(Model):
     postal_code = Property()
@@ -10,3 +15,4 @@ class Location(Model):
     unit = Property()
 
     has_prop_type = RelatedTo(SpecificPropType)
+
