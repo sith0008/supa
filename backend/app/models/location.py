8f7fff257c9d2a2b30d063b3fbc0f041eedbcf34
hanzhe <sith0008@e.ntu.edu.sg>
2021-01-24 16:18:55 +0800

    create ogm models to map python obj to neo4j nodes

--- backend/app/models/location.py
+++ backend/app/models/location.py
@@ -1,4 +1,12 @@
-# TODO: add data model for location
+from py2neo.ogm import Model, Property, RelatedTo
+from app.models.property_type import SpecificPropType # noqa
 
-class Location:
-    pass
+
+class Location(Model):
+    postal_code = Property()
+    building_name = Property()
+    lot_number = Property()
+    floor = Property()
+    unit = Property()
+
+    has_prop_type = RelatedTo(SpecificPropType)
--- backend/app/models/past_case.py
+++ backend/app/models/past_case.py
@@ -1,4 +1,16 @@
-# TODO: add data model for past case here
+from py2neo.ogm import Model, Property, RelatedTo
+from app.models.use_class import SpecificUseClass # noqa
+from app.models.location import Location # noqa
 
-class PastCase:
-    pass
+
+class PastCase(Model):
+    case_id = Property()
+    proposed_use_desc = Property()
+    gfa = Property()
+    decision = Property()
+    evaluation = Property()
+
+    __primarykey__ = "case_id"
+
+    has_use_class = RelatedTo(SpecificUseClass)
+    located_in = RelatedTo(Location)
