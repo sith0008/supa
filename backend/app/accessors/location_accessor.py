2283a346ad923f2e61451b57b1d361ec4c8d58bc
hanzhe <sith0008@e.ntu.edu.sg>
2021-01-25 15:05:11 +0800

    add basic CRUD operations for location

--- backend/app/accessors/location_accessor.py
+++ backend/app/accessors/location_accessor.py
@@ -1,3 +1,19 @@
+from typing import NamedTuple
+from app.models.location import Location # noqa
+
+
 class LocationAccessor:
     def __init__(self, graph):
         self.graph = graph
+    # location is keyed by postal code, floor, unit
+    def get_location_by_key(self, location_key: NamedTuple):
+        raise NotImplementedError
+
+    def insert(self, location: Location):
+        raise NotImplementedError
+
+    def update(self, location: Location):
+        raise NotImplementedError
+
+    def delete(self, location_key: NamedTuple):
+        raise NotImplementedError
