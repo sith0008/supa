bf60adcdffe4721e7ab2eb50cdc3d97577c15630
hanzhe <sith0008@e.ntu.edu.sg>
2021-01-25 11:49:24 +0800

    add logging custom handler

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,15 @@
 from app import supa
 from app.services import cases_service
 from flask import request, jsonify
+import logging
+
+log = logging.getLogger('root')
 
 @supa.route("/", methods=['GET'])
 def test():
+    log.info("Received request")
+    log.info("Processing request")
+    log.info("Processed request")
     return "hello world."
 
 # TODO: add more routes here when business logic is done
