6f2f64c5038a573b48ac5058da4ecd01399d026a
Han Zhe Sit <42863163+sith0008@users.noreply.github.com>
2021-01-25 09:42:35 +0800

    setup database on docker (#6)

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
-from app.database import db
+from app.database import sql_db # noqa
+import os
+
 
 supa = Flask(__name__)
-db.init_app(supa)
+supa.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQL_DATABASE_URI")
+sql_db.init_app(supa)
--- backend/app/business/cases_service.py
+++ backend/app/business/cases_service.py
@@ -1,3 +1,4 @@
 class CasesService:
     def __init__(self, engine):
         self.engine = engine
+
