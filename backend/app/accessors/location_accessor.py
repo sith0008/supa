160d80d0aac509978264e4aa63ec11edc7e264ae
hanzhe <sith0008@e.ntu.edu.sg>
2021-01-25 15:10:45 +0800

    add secondary getter

--- backend/app/accessors/location_accessor.py
+++ backend/app/accessors/location_accessor.py
@@ -1,5 +1,6 @@
 from typing import NamedTuple
 from app.models.location import Location # noqa
+from app.models.property_type import SpecificPropTypeEnum # noqa
 
 
 class LocationAccessor:
@@ -7,6 +8,9 @@ class LocationAccessor:
         self.graph = graph
     # location is keyed by postal code, floor, unit
     def get_location_by_key(self, location_key: NamedTuple):
+        raise NotImplementedError
+
+    def get_locations_related_to_prop_type(self, prop_type: SpecificPropTypeEnum):
         raise NotImplementedError
 
     def insert(self, location: Location):
@@ -17,3 +21,4 @@ class LocationAccessor:
 
     def delete(self, location_key: NamedTuple):
         raise NotImplementedError
+
