cc44e5fe897ab8a6f5a344be9922d5a5aa942697
hanzhe <sith0008@e.ntu.edu.sg>
2021-01-25 17:01:00 +0800

    rework location OGM

--- backend/app/models/location.py
+++ backend/app/models/location.py
@@ -1,12 +1,18 @@
-from py2neo.ogm import Model, Property, RelatedTo
+# from py2neo.ogm import Model, Property, RelatedTo
 from app.models.property_type import SpecificPropType # noqa
 
 
-class Location(Model):
-    postal_code = Property()
-    building_name = Property()
-    lot_number = Property()
-    floor = Property()
-    unit = Property()
-
-    has_prop_type = RelatedTo(SpecificPropType)
+class Location:
+    def __init__(
+            self,
+            postal_code: int = None,
+            floor: int = None,
+            unit: int = None,
+            lot_number: str = None,
+            building_name: str = None
+    ):
+        self.postal_code = postal_code
+        self.floor = floor
+        self.unit = unit
+        self.lot_number = lot_number
+        self.building_name = building_name
