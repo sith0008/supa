2f4c5e5d2b34d6fdb1180d683a23bd2e04d8df35
Jay Sean <jaysean.yeo@gmail.com>
2021-01-26 21:35:45 +0800

    implement CRUD

--- backend/app/accessors/guidelines_accessor.py
+++ backend/app/accessors/guidelines_accessor.py
@@ -9,28 +9,40 @@ class GuidelinesAccessor:
     def __init__(self, session):
         self.session = session
 
-    def get_guidelines(self, filter_map: Dict):
-        q = self.session.query(Guideline)
-        for k, v in filter_map.items():
-            log.info(k, getattr(Guideline, k), v)
-            # q = q.filter(getattr(Guideline, k) == v)
-        guidelines = q.as_dict()
-        log.info(guidelines)
-        # guidelines = list(q)
-        return guidelines
-
     def create(self, guideline: Guideline):
-        log.info(f"Inserting guideline {guideline}")
+        log.info(f"Inserting guideline")
         self.session.add(guideline)
-        log.info(f"Successfully inserted guideline {guideline}")
+        log.info(f"Successfully inserted guideline")
         self.session.commit()
         return guideline.business_use_type, guideline.property_type, guideline.unit_type, guideline.conditions
 
-    def read(self, guideline_key: NamedTuple):
-        raise NotImplementedError
+    def read(self, filter_map: Dict):
+        log.info(f"Retrieving guideline")
+        q = self.session.query(Guideline)
+        for k, v in filter_map.items():
+            q = q.filter(getattr(Guideline, k) == v)
+        guidelines = q.first()
+        if guidelines:
+            guidelines = guidelines.as_dict()
+            log.info(f"Successfully retrieved guideline")
+            return guidelines
+        else:
+            return None
 
-    def update(self, guideline: Guideline):
-        raise NotImplementedError
+    def update(self, pri_map: Dict, upd_map: Dict):
+        log.info(f"Updating guideline")
+        q = self.session.query(Guideline)
+        for k, v in pri_map.items():
+            q = q.filter(getattr(Guideline, k) == v)
+        q.update(upd_map)
+        log.info(f"Successfully updated guideline")
+        self.session.commit()
 
-    def delete(self, guideline_key: NamedTuple):
-        raise NotImplementedError
+    def delete(self, pri_map: Dict):
+        log.info(f"Deleting guideline")
+        q = self.session.query(Guideline)
+        for k, v in pri_map.items():
+            q = q.filter(getattr(Guideline, k) == v)
+        q.delete()
+        log.info(f"Successfully deleted guideline")
+        self.session.commit()
