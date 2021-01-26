b221ef385fbf81a5993b05a33f809d1c4efa86bb
hanzhesit <sith0008@e.ntu.edu.sg>
2021-01-26 18:10:35 +0800

    setup ingestion entrypoint

--- ingestion/run.py
+++ ingestion/run.py
@@ -1,1 +1,28 @@
-# TOOD: add ingestion script
+import argparse
+
+def main(init_sql, init_graph):
+    if init_sql:
+        # TODO: run init guideline method
+        raise NotImplementedError
+
+    if init_graph:
+        # TODO: run init graph nodes methods
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--sql",
+        action='store_true',
+        help="whether to initialise the MySQL database"
+    )
+    parser.add_argument(
+        "-g",
+        "--graph",
+        action='store_true',
+        help="whether to initialise the neo4j database"
+    )
+    args = parser.parse_args()
+    main(args.sql, args.graph)
