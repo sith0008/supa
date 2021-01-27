725538bf5b45b08ec720bc63459042c9e1c9d991
hanzhesit <sith0008@e.ntu.edu.sg>
2021-01-27 16:38:57 +0800

    add use class ingestor

--- ingestion/ingestors/use_class_ingestor.py
+++ ingestion/ingestors/use_class_ingestor.py
@@ -1,5 +1,25 @@
+from ingestors.use_class import generic_use_classes, specific_use_class_map  # noqa
+import requests
+import json
+
+
 class UseClassIngestor:
     def __init__(self, host, endpoint):
         self.url = host + endpoint
 
-    # TODO: add methods here
+    def insert_generic(self):
+        for generic_use_class in generic_use_classes:
+            data = {
+                "type": "Generic",
+                "name": generic_use_class
+            }
+            _ = requests.post(self.url, data=json.dumps(data))
+
+    def insert_specific(self):
+        for specific, generic in specific_use_class_map.items():
+            data = {
+                "type": "Specific",
+                "name": specific,
+                "generic": generic
+            }
+            _ = requests.post(self.url, data=json.dumps(data))
