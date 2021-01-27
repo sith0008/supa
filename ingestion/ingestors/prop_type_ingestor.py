938497cd8f3cba028e27a30ba77af46c8de761e0
hanzhesit <sith0008@e.ntu.edu.sg>
2021-01-27 16:46:16 +0800

    add prop type ingestor

--- ingestion/ingestors/prop_type_ingestor.py
+++ ingestion/ingestors/prop_type_ingestor.py
@@ -1,5 +1,25 @@
+import requests
+import json
+from ingestors.property_type import generic_property_types, specific_property_type_map # noqa
+
+
 class PropertyTypeIngestor:
     def __init__(self, host, endpoint):
         self.url = host + endpoint
 
-    # TODO: add methods here
+    def insert_generic(self):
+        for generic_property_type in generic_property_types:
+            data = {
+                "type": "Generic",
+                "name": generic_property_type
+            }
+            _ = requests.post(self.url, data=json.dumps(data))
+
+    def insert_specific(self):
+        for specific, generic in specific_property_type_map.items():
+            data = {
+                "type": "Specific",
+                "name": specific,
+                "generic": generic
+            }
+            _ = requests.post(self.url, data=json.dumps(data))
