d20e6f94bf0608cfb66914c8dfda2fe583b4d643
Jay Sean <jaysean.yeo@gmail.com>
2021-01-27 23:04:50 +0800

    tidy up

--- backend/app/models/guidelines.py
+++ backend/app/models/guidelines.py
@@ -1,13 +1,9 @@
 from app.database import sql_db  # noqa
 from sqlalchemy import (
-    create_engine,
     Column,
     Text,
-    Integer,
     VARCHAR
 )
-from sqlalchemy.orm import sessionmaker
-import csv
 
 OutcomePriority = {
     'Not Allowed': 0,
@@ -25,22 +21,6 @@ class Guideline(sql_db.Model):
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-    # def __repr__(self):
-    #     return "<Guideline(" \
-    #            "Business Use Type='%s'," \
-    #            "Property Type='%s'," \
-    #            "Unit Type='%s'," \
-    #            "Conditions'%s'," \
-    #            "Outcome'%s'," \
-    #            "Remarks='%s')>" % (
-    #                self.business_use_type,
-    #                self.property_type,
-    #                self.unit_type,
-    #                self.conditions,
-    #                self.outcome,
-    #                self.remarks
-    #            )
 
     business_use_type = Column(VARCHAR(100), primary_key=True)
     property_type = Column(VARCHAR(100), primary_key=True)
