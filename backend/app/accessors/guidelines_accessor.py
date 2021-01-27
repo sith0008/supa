5f3beffe94645e960520aa2fbe8d465bb4feff7e
Jay Sean <jaysean.yeo@gmail.com>
2021-01-27 23:04:03 +0800

    log after commit, return mutiple guidelines in read

--- backend/app/accessors/guidelines_accessor.py
+++ backend/app/accessors/guidelines_accessor.py
@@ -12,8 +12,8 @@ class GuidelinesAccessor:
     def create(self, guideline: Guideline):
         log.info(f"Inserting guideline")
         self.session.add(guideline)
-        log.info(f"Successfully inserted guideline")
         self.session.commit()
+        log.info(f"Successfully inserted guideline")
         return guideline.business_use_type, guideline.property_type, guideline.unit_type, guideline.conditions
 
     def read(self, filter_map: Dict):
@@ -21,13 +21,10 @@ class GuidelinesAccessor:
         q = self.session.query(Guideline)
         for k, v in filter_map.items():
             q = q.filter(getattr(Guideline, k) == v)
-        guidelines = q.first()
-        if guidelines:
-            guidelines = guidelines.as_dict()
-            log.info(f"Successfully retrieved guideline")
-            return guidelines
-        else:
-            return None
+        guidelines = q.all()
+        guidelines = [g.as_dict() for g in guidelines]
+        log.info(f"Successfully retrieved guideline")
+        return guidelines
 
     def update(self, pri_map: Dict, upd_map: Dict):
         log.info(f"Updating guideline")
@@ -35,8 +32,8 @@ class GuidelinesAccessor:
         for k, v in pri_map.items():
             q = q.filter(getattr(Guideline, k) == v)
         q.update(upd_map)
-        log.info(f"Successfully updated guideline")
         self.session.commit()
+        log.info(f"Successfully updated guideline")
 
     def delete(self, pri_map: Dict):
         log.info(f"Deleting guideline")
@@ -44,5 +41,5 @@ class GuidelinesAccessor:
         for k, v in pri_map.items():
             q = q.filter(getattr(Guideline, k) == v)
         q.delete()
-        log.info(f"Successfully deleted guideline")
         self.session.commit()
+        log.info(f"Successfully deleted guideline")
