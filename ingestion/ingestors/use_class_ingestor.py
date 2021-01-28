c894449d9d1f2d8d9c9ac700374b79f87a52feb7
hanzhe <sith0008@e.ntu.edu.sg>
2021-01-28 16:01:14 +0800

    test and fix use class ingestor

--- ingestion/ingestors/use_class_ingestor.py
+++ ingestion/ingestors/use_class_ingestor.py
@@ -1,6 +1,9 @@
 from ingestors.use_class import generic_use_classes, specific_use_class_map  # noqa
 import requests
 import json
+import logging
+
+log = logging.getLogger('root')
 
 
 class UseClassIngestor:
@@ -9,17 +12,27 @@ class UseClassIngestor:
 
     def insert_generic(self):
         for generic_use_class in generic_use_classes:
-            data = {
-                "type": "Generic",
-                "name": generic_use_class
+            headers = {
+                'content-type': 'application/json'
             }
-            _ = requests.post(self.url, data=json.dumps(data))
+            data = {
+                'type': 'Generic',
+                'name': generic_use_class
+            }
+            res = requests.put(self.url, headers=headers, data=json.dumps(data))
+            log.debug(f"request body: {res.request.body}")
+            log.debug(f"status code: {res.status_code}")
 
     def insert_specific(self):
         for specific, generic in specific_use_class_map.items():
-            data = {
-                "type": "Specific",
-                "name": specific,
-                "generic": generic
+            headers = {
+                'content-type': 'application/json'
             }
-            _ = requests.post(self.url, data=json.dumps(data))
+            data = {
+                'type': 'Specific',
+                'name': specific,
+                'generic': generic
+            }
+            res = requests.put(self.url, headers=headers, data=json.dumps(data))
+            log.debug(f"request body: {res.request.body}")
+            log.debug(f"status code: {res.status_code}")
