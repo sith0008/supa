d8e323bbdb22436b90ba99b62d4ad0bfc73e0b60
hanzhe <sith0008@e.ntu.edu.sg>
2021-02-01 21:13:43 +0800

    implement kg chatbot service

--- backend/app/business/kg_chatbot_service.py
+++ backend/app/business/kg_chatbot_service.py
@@ -29,16 +29,19 @@ class KnowledgeGraphChatbotService:
                           ):
         tx = self.graph.begin()
         # get prop type from location
-        location_key = LocationKey(postal_code, floor, unit)
-        specific_property_type = self.property_type_service.get_specific_by_location(location_key)
+        specific_property_type = self.property_type_service.get_specific_by_location(postal_code, floor, unit)
         # get generics from specifics
         generic_property_type = self.property_type_service.get_generic_by_specific(specific_property_type)
         generic_use_class = self.use_class_service.get_generic_by_specific(specific_use_class)
         # get similar cases
         exact_similar_cases_result = self.cases_service.get_similar_case_exact(specific_use_class, specific_property_type)
         extended_similar_cases_result = self.cases_service.get_similar_case_extended(specific_use_class, generic_use_class, specific_property_type, generic_property_type)
-        similar_cases_result = exact_similar_cases_result + extended_similar_cases_result
-        return similar_cases_result
+        if not exact_similar_cases_result:
+            return extended_similar_cases_result
+        elif not extended_similar_cases_result:
+            return exact_similar_cases_result
+        else:
+            return exact_similar_cases_result + extended_similar_cases_result
 
     def generate_use_class_clarifications(self, specific_use_class_name: str):
         use_class = self.use_class_service.get_by_name(specific_use_class_name)
@@ -52,18 +55,17 @@ class KnowledgeGraphChatbotService:
                                       unit: int
                                       ):
         location_key = LocationKey(postal_code, floor, unit)
-        # get property type
         property_type = self.property_type_service.get_specific_by_location(location_key)
-        # get unit type (NOT IMPLEMENTED)
-        unit_type = self.location_service.get_unit_type(postal_code, floor, unit)
-        # get condition type (NOT IMPLEMENTED)
-        condition = self.location_service.get_condition(postal_code)
-        # query mysql DB
+        location = self.location_service.get_location({
+            "postal_code": postal_code,
+            "floor": floor,
+            "unit": unit
+        })
         guidelines = self.guidelines_service.get_guidelines({
             "business_use_type": use_class,
             "property_type": property_type,
-            "unit_type": unit_type,
-            "conditions": condition
+            "unit_type": location["unit_type"],
+            "conditions": location["condition"]
         })
         return guidelines
 
