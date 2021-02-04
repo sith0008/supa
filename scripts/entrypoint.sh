375817686ca14e2f236a51c08f641fc8f110d615
hanzhesit <sith0008@e.ntu.edu.sg>
2021-02-04 14:58:34 +0800

    implement script to load docker image

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,1 +1,14 @@
 # TODO: write script to start docker images, create volume and load data into DB if needed
+#!/usr/bin/env bash
+set -e
+
+IMAGES_DIRECTORY='./images'
+
+load_images()
+{
+  for image in "$1"/*.gz; do
+    docker load < "$image"
+  done
+}
+
+load_images $IMAGES_DIRECTORY
