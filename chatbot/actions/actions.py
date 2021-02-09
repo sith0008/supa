405a5f398d79e268cc693c62e5f849951ca4732c
hanzhesit <sith0008@e.ntu.edu.sg>
2021-02-09 16:27:49 +0800

    implement some custom actions

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,34 +1,3 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-# TODO: add actions here, instantiate the 3 classes in the api folder
-import logging
-import json
-import requests
 from rasa_sdk import Action, Tracker
 from rasa_sdk.forms import FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
@@ -39,27 +8,109 @@ from rasa_sdk.events import (
     EventType,
     FollowupAction,
 )
-from actions.api.knowledge_graph import KnowledgeGraphAPI
-
+from typing import Text, Dict, Any, List
+from actions.api.knowledge_graph import KnowledgeGraphAPI # noqa
+from actions.api.location import LocationAPI # noqa
+from actions.api.validator import GeneralValidator # noqa
 
 class ValidateVerifyForm(FormValidationAction):
     '''
-    validate all form fields
+    validate all verify form fields
     '''
-    raise NotImplementedError
+    def name(self):
+        return "validate_verify_form"
+
+    def validate_use_class(self,
+                           value: Text,
+                           dispatcher: CollectingDispatcher,
+                           tracker: Tracker,
+                           domain: Dict[Text, Any]
+                           ):
+        if KnowledgeGraphAPI.is_valid_use_class(value):
+            return {"use_class": value}
+        else:
+            dispatcher.utter_message(template="utter_invalid_use_class")
+
+    # TODO: uncomment after GeneralValidator is implemented
+    def validate_gfa(self,
+                           value: Text,
+                           dispatcher: CollectingDispatcher,
+                           tracker: Tracker,
+                           domain: Dict[Text, Any]
+                           ):
+        if GeneralValidator.is_valid_float(value):
+            return {"gfa": value}
+        else:
+            dispatcher.utter_message(template="utter_invalid_gfa")
+    # TODO: uncomment after location DB is implemented
+    # def validate_postal_code(self,
+    #                        value: Text,
+    #                        dispatcher: CollectingDispatcher,
+    #                        tracker: Tracker,
+    #                        domain: Dict[Text, Any]
+    #                        ):
+    #     if LocationAPI.is_valid_postal_code(value):
+    #         return {"postal_code": value}
+    #     else:
+    #         dispatcher.utter_message(template="utter_invalid_postal_code")
+    # TODO: uncomment after GeneralValidator is implemented
+    def validate_floor(self,
+                           value: Text,
+                           dispatcher: CollectingDispatcher,
+                           tracker: Tracker,
+                           domain: Dict[Text, Any]
+                           ):
+        if GeneralValidator.is_valid_integer(value):
+            return {"floor": value}
+        else:
+            dispatcher.utter_message(template="utter_invalid_floor")
+
+    def validate_unit(self,
+                           value: Text,
+                           dispatcher: CollectingDispatcher,
+                           tracker: Tracker,
+                           domain: Dict[Text, Any]
+                           ):
+        if GeneralValidator.is_valid_integer(value):
+            return {"unit": value}
+        else:
+            dispatcher.utter_message(template="utter_invalid_unit")
 
 
 class ActionShowUseClasses(Action):
     '''
     Retrieve all specific use class from KG
     '''
-    raise NotImplementedError
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]
+            ) -> List[EventType]:
+        use_class_list = KnowledgeGraphAPI.get_all_use_classes()
+        dispatcher.utter_message(
+            text=f"These are the use classes that we use: \n {use_class_list}"
+        )
+        return []
 
 class ActionGetSimilarCases(Action):
     '''
     Retrieve similar cases based on use class and prop type
     '''
-    raise NotImplementedError
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]
+            ) -> List[EventType]:
+        similar_cases_list = KnowledgeGraphAPI.get_similar_cases()
+        processed_similar_cases_list = self.process_similar_cases_list(similar_cases_list)
+        dispatcher.utter_message(
+            text=f"These are the past cases similar to your application: \n {processed_similar_cases_list}"
+        )
+        return [SlotSet("similar_cases_list"), similar_cases_list]
+
+    # TODO: to add processing code, for readability
+    def process_similar_cases_list(self, similar_cases_list):
+        return similar_cases_list
 
 
 class ActionGetAddresses(Action):
@@ -67,7 +118,24 @@ class ActionGetAddresses(Action):
     Retrieve addresses based on postal code
     Sets address_list slot to contain retrieved addresses
     '''
-    raise NotImplementedError
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]
+            ) -> List[EventType]:
+        postal_code = tracker.get_slot("postal_code")
+        address_list = LocationAPI.get_addresses_from_postal_code(postal_code)
+        if len(address_list) >= 1:
+            dispatcher.utter_message(
+                text=f"I found {len(address_list)} addresses for this postal code. "
+                     f"Could you confirm which one is it? \n {address_list}"
+            )
+        else:
+            dispatcher.utter_message(
+                text=f"I found the following address for this postal code. "
+                     f"Could you confirm if this is correct? \n {address_list[0]}"
+            )
+        return [SlotSet("address_list"), address_list]
 
 
 class ActionVerifyProposal(Action):
