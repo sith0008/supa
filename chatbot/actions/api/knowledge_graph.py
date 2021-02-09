dbc672b768bf1e4ae89be3bcdf48fe3e4f8f8b14
hanzhesit <sith0008@e.ntu.edu.sg>
2021-02-09 16:25:49 +0800

    add kg api

--- chatbot/actions/api/knowledge_graph.py
+++ chatbot/actions/api/knowledge_graph.py
@@ -1,24 +1,53 @@
 # TODO: add class to make API calls for Knowledge Graph DB
+import requests
+import json
 
 class KnowledgeGraphAPI:
     def __init__(self, url):
         self.url = url
 
+    @staticmethod
     def get_similar_cases(self, use_class: str, land_use_type: str):
-        raise NotImplementedError
+        # TODO: implement after kg chatbot service branch is merged
+        headers = {
+            'content-type': 'application/json'
+        }
+        data = {
+            "land_use_type": "",
+            "use_class": ""
+        }
+        return ""
 
+    @staticmethod
     def get_all_use_classes(self):
-        raise NotImplementedError
+        headers = {
+            'content-type': 'application/json'
+        }
+        data = {
+            "type": "multiple",
+            "query": "Specific"
+        }
+        res = requests.get(url=self.url, headers=headers, data=json.dumps(data))
+        # TODO: add processing, format to a readable list
+        return res
 
-    def validate_use_class(self, use_class: str):
-        raise NotImplementedError
+    @staticmethod
+    def is_valid_use_class(self, use_class: str):
+        headers = {
+            'content-type': 'application/json'
+        }
+        data = {
+            "type": "single",
+            "query": use_class
+        }
+        res = requests.get(url=self.url, headers=headers, data=json.dumps(data))
+        return res is not None
 
-    def validate_postal_code(self, postal_code: int):
-        raise NotImplementedError
-
+    @staticmethod
     def get_locations(self, postal_code: int, floor: int, unit: int):
         raise NotImplementedError
 
+    @staticmethod
     def get_property_type(self, postal_code: int, floor: int, unit: int, block: int, road: str):
         # query location DB, check booleans + land_use_type (rs)
         # create in-memory map here
