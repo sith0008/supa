215bcf4a1a470243d8dae4b40bec1ee856f91282
hanzhe <sith0008@e.ntu.edu.sg>
2021-03-14 21:05:10 +0800

    update use class model

--- backend/app/models/use_class.py
+++ backend/app/models/use_class.py
@@ -6,7 +6,19 @@ class GenericUseClass:
         self.name = name
 
 class SpecificUseClass:
-    def __init__(self, name: str = None):
+    def __init__(self,
+                 name: str = None,
+                 definition: str = None,
+                 requirements: str = None
+                 ):
+        self.name = name
+        self.definition = definition
+        self.requirements = requirements
+
+class SpecificUseClassExample:
+    def __init__(self,
+                 name: str = None,
+                 ):
         self.name = name
 
 class UseClassType(Enum):
